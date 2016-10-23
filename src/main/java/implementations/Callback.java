38feefcd7ef4c6a57217a8fc55d33ce1e0c6a43a
AdamPiech <adampiech7@op.pl>
2016-10-23 15:24:17 +0200

    Added functionality to Callback.

--- src/main/java/implementations/Callback.java
+++ src/main/java/implementations/Callback.java
@@ -3,15 +3,23 @@ package implementations;
 import interfaces.ICallback;
 
 import java.util.Optional;
+import java.util.Scanner;
 
 /**
  * Created by Adam Piech on 2016-10-23.
  */
 public class Callback implements ICallback {
 
+    private Scanner scanner = new Scanner(System.in);
+
     @Override
     public Optional<String> getResult(Boolean result) {
-        return null;
+        System.out.println("Czy chcesz storzyć nową sciezke (T/N)?");
+        if (scanner.nextLine().equalsIgnoreCase("T")) {
+            System.out.print("Podaj nowa sciezke:");
+            return Optional.of(scanner.nextLine());
+        }
+        return Optional.empty();
     }
 
 }
