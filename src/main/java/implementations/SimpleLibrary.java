6b75b098e7a593cdf8c3f10237ae629ee90986a5
AdamPiech <adampiech7@op.pl>
2016-10-23 15:47:02 +0200

    Library refactoring.

--- src/main/java/implementations/SimpleLibrary.java
+++ src/main/java/implementations/SimpleLibrary.java
@@ -16,17 +16,20 @@ public class SimpleLibrary implements ILibrary {
     @Override
     public void saveToFile(String file, String successMessage, String failtureMessage, ICallback callback) {
         System.out.println(LOG + SIMPLE_LOG_MESSAGE);
-        boolean result = false;
+        callback
+                .getResult(saveExecute(file))
+                .ifPresent(newFile -> saveToFile(newFile, successMessage, failtureMessage, callback));
+    }
 
-        do {
-            try (PrintWriter printWriter = new PrintWriter(file)) {
-                printWriter.write(TEXT_TO_SAVE);
-                System.out.println(SUCCESS);
-                result = true;
-            } catch (IOException exception) {
-                System.out.println(FAILURE);
-            }
-        } while (!callback.getResult(result).equals(successMessage));
+    private boolean saveExecute(String file) {
+        try (PrintWriter printWriter = new PrintWriter(file)) {
+            printWriter.write(TEXT_TO_SAVE);
+            System.out.println(SUCCESS);
+            return true;
+        } catch (IOException exception) {
+            System.out.println(FAILURE);
+        }
+        return false;
     }
 
 }
